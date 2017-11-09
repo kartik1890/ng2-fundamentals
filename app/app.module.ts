a6752d15b48938586663b00509e1a9e0329512f0
Kurra <kartik.kurra@capgemini.com>
2017-11-09 13:07:30 +0530

    templateUrl

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,17 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 //import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
-import { TextComponent } from './text.component';
+//import { TextComponent } from './text.component';
+import {ProductComponent} from './product-list.component'
 
 @NgModule({
 declarations: [
-AppComponent, TextComponent
+AppComponent,  ProductComponent
 ],
 imports: [
 BrowserModule,
 //HttpClientModule
 ],
-bootstrap : [AppComponent,TextComponent]
+bootstrap : [AppComponent, ProductComponent]
 
 })
 export class AppModule{ } 
